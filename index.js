bbc86a44a2af7ba01b88d4f577a56ffd109c3ee4
conni <c.buschka@arcusx.com>
2017-09-01 08:37:16 +0200

    Version 1.0.0.

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const PLUGIN_NAME = 'gulp-if-any-newer'
+
+const gutil = require('gulp-util')
+const through = require('through2')
+const glob = require('glob')
+const fs = require('fs')
+const path = require('path')
+
+function getMinModTime(dir, filter) {
+	var minModTime = Number.MAX_SAFE_INTEGER
+	var files = glob.sync(filter, {cwd: dir, absolute: true, nodir: true})
+	for(let i=0; i<files.length; ++i) {
+		let stats = fs.statSync(files[i])
+		minModTime = Math.min(stats.mtime, minModTime)
+	}
+	return minModTime
+}
+
+const gulpIfAnyNewer = (dest, opts) => {
+	if (!dest) {
+		throw new gutil.PluginError(PLUGIN_NAME, '"dest" required')
+	}
+
+	opts = Object.assign({
+		cwd: process.cwd(),
+		filter: '**/*'
+	}, opts)
+
+	const destAbs = path.resolve(opts.cwd, dest)
+	const destMinModTime = getMinModTime(destAbs, opts.filter)
+
+	const buffer = []
+	let open = destMinModTime === Number.MAX_SAFE_INTEGER
+	return through.obj(function (chunk, enc, callback) {
+
+		if( !open && chunk.path ) {
+			let srcPath = chunk.path
+			let stats = fs.statSync(srcPath)
+			if( stats.mtime > destMinModTime ) {
+				open = true
+			}
+		}
+
+		if( !open ) {
+			buffer.push(chunk)
+		} else {
+			if( buffer.length > 0 ) {
+				for(let i=0; i<buffer.length; ++i) {
+					const chunk = buffer[i]
+					this.push(chunk)
+				}
+				buffer = []
+			}
+			this.push(chunk)
+		}
+
+		callback()
+	});
+};
+
+module.exports = gulpIfAnyNewer;
--- readme.md
+++ readme.md
@@ -1,11 +1,29 @@
-# gulp-if-any-changed
+# gulp-if-any-newer
 
-> Pass through all files if any file has changed after any dest file.
+#### Pass through all source files if any source file is newer than any dest file.
 
-#### Work In Progress!
+## Usage
+
+```js
+const gulp = require('gulp');
+const ifAnyNewer = require('gulp-if-any-newer');
+const anyConsumer = require('gulp-any-consumer');
+
+const SRC = 'src/*.js';
+const DEST = 'dist';
+
+gulp.task('default', () =>
+  gulp.src(SRC)
+    .pipe(ifAnyNewer(DEST, { filter: '**/*' }))
+    // anyConsumer will only get the files feeded in
+    // if any source file is newer than any file in dest
+    .pipe(anyConsumer())
+    .pipe(gulp.dest(DEST));
+);
+```
 
 ## License
 
-Copyriht (c) 2017 [Cornelius Buschka](https://github.com/cbuschka)
+Copyriht (c) 2017 by [Cornelius Buschka](https://github.com/cbuschka)
 
 [MIT License](license)
